250d3e7470fe72a19d9db853442839f00fa884b8
chaelynjang <chaelynjang@lotte.net>
2023-05-02 17:04:16 +0900

    travis 배포 설정 추가

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: 'cl',
-    database: 'myapp'
+    host: process.env.MYSQL_HOST, //'mysql'
+    user: process.env.MYSQL_USER, //'root'
+    password: process.env.MYSQL_ROOT_PASSWORD, //'cldockerfullstack'
+    database: process.env.MYSQL_DATABASE, //'myapp'
+    port: process.env.MYSQL_PORT // 3306
 });
 // 다른 파일에서도 해당 pool을 쓸 수 있도록 export
 exports.pool = pool;
